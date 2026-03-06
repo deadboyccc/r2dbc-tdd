7a6a63824ffafeec9c998980ceff484a0a1372db
Ahmed M. Ali <ahmed.mohammed.ali9999@gmail.com>
2026-03-07 01:30:01 +0300

    Add Vavr dependency and implement tests for stream operations and immutable collections

--- src/test/java/dev/dead/r2dbctdd/R2dbcTddApplicationTests.java
+++ src/test/java/dev/dead/r2dbctdd/R2dbcTddApplicationTests.java
@@ -1,13 +1,35 @@
 package dev.dead.r2dbctdd;
 
+import io.vavr.collection.Stream;
+import lombok.val;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+//@SpringBootTest
 class R2dbcTddApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void testVavr() {
+        val a = Stream.of("a", "b", "c")
+                .map(String::toUpperCase)
+                .asJava();
+        System.out.println(a);
+
+    }
+
+    @Test
+    void immutableCollection() {
+        List<String> list = List.of("a", "b", "c");
+        val a = Collections.unmodifiableCollection(list);
+        assertThrows(UnsupportedOperationException.class, () -> a.add("d"));
     }
 
 }
